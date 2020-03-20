e9d9e27ba10667ae55d642cb594e97f15b558021
Prasanta Karmakar <prasanta.1987@hotmail.com>
2020-03-20 10:44:00 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,61 @@
+const scrapeIt = require("scrape-it")
+const axios = require('axios');
+
+const fetchData = () => {
+    scrapeIt("https://www.mohfw.gov.in/", { title: "tr" })
+        .then(({ data, response }) => {
+            let dataValue = []
+            let chunk = []
+            let finalData = []
+            let breaks = 0;
+
+            // console.log(`Status Code: ${response.statusCode}`)
+            let dataArray = data.title.split()
+            dataArray.map(values => {
+                values.split("\n").map(value => dataValue.push(value.trim()))
+            })
+            // console.log(dataValue.splice(21, breaks))
+
+            for (let i = 0; i < dataValue.length; i++) {
+                chunk.push(dataValue[i])
+                breaks++
+                if (breaks > 6) {
+                    finalData.push(chunk)
+                    chunk = []
+                    breaks = 0
+                }
+            }
+
+            finalData.shift()
+            finalData.pop()
+            finalData.map(popData => { popData.pop() })
+            finalData.map(shiftData => shiftData.shift())
+            dataObject = {}
+            dataObjectChunk = {}
+            finalData.map(entries => {
+                dataObjectChunk = {
+                    "State": entries[0],
+                    "Infected(Indian)": entries[1],
+                    "Infected(Foreigner)": entries[2],
+                    "Cured": entries[3],
+                    "Death": entries[4]
+                }
+                dataObject[entries[0]] = dataObjectChunk
+            })
+
+            // const fbUrl = 'https://asia-east2-pran-home.cloudfunctions.net/api/covid/'
+            const fbUrl = 'http://localhost:5000/pran-home/asia-east2/api/covid/'
+            axios({
+                method: 'post',
+                url: fbUrl,
+                data: dataObject
+            })
+                .then(res => console.log(res.data))
+                .catch(err => console.log(err))
+        })
+        .catch(err => console.log(err))
+}
+
+fetchData()
+
+setInterval(fetchData, 1800000)
